d90f9baa82c72bc5d82b1ce1ef0ce39a5de3217e
Chucks Nwosu <chucksn611@gmail.com>
2023-03-08 14:33:20 -0600

    main quiz popup added

--- src/components/quiz-instructions.jsx
+++ src/components/quiz-instructions.jsx
@@ -1,18 +1,44 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { resetQuizStart } from "../redux/slices/startQuiz-slice";
 
 function QuizInstructions() {
+  const dispatch = useDispatch();
   const quizHasStarted = useSelector((state) => state.quizHasStarted);
+
+  const handleExit = () => {
+    dispatch(resetQuizStart());
+  };
   return (
     <>
       {quizHasStarted && (
-        <div className=" w-4/5 md:w-3/5 lg:w-1/2 bg-gray-300 p-4 rounded-lg flex flex-col justify-between ">
+        <div className="quiz-rules w-4/5 md:w-3/5 lg:w-1/2 bg-gray-300 p-4 rounded-lg flex flex-col justify-between ">
           <div className="border-b-2 border-zinc-400/20 m-4 pb-2">
-            <span className="quiz-title block font-ubuntu font-medium text-zinc-800/70 text-center text-xl md:text-2xl lg:text-3xl ">
-              BRAINER QUIZ
+            <span className="block font-itim text-center text-xl md:text-2xl lg:text-3xl text-zinc-900/70">
+              Quiz Rules
             </span>
-            <span className="block font-itim text-center md:text-2xl text-lg text-zinc-900/70">
-              Instructions
-            </span>
+          </div>
+          <div className="rule-list border-b-2 border-zinc-400/20 px-8 flex flex-col items-center pb-2">
+            <ol className=" list-decimal text-zinc-900/70 md:text-lg font-robotoMono font-medium">
+              <li>
+                You have only <span className="text-sky-500">15 seconds</span>{" "}
+                to answer each question.
+              </li>
+              <li>Once an answer is selected, it can't be undone. </li>
+              <li>An option cannot be selected once the timer goes off.</li>
+              <li>You can't exit from the Quiz while you're still playing.</li>
+              <li>Points are gained based on correct answers.</li>
+            </ol>
+          </div>
+          <div className="btn-container mt-2 flex justify-center">
+            <button
+              className="exit p-2 bg-sky-600 text-white rounded-lg lg:hover:bg-sky-500 m-2"
+              onClick={handleExit}
+            >
+              Exit Quiz
+            </button>
+            <button className="continue p-2 bg-sky-600 text-white rounded-lg lg:hover:bg-sky-500 m-2">
+              Continue
+            </button>
           </div>
         </div>
       )}
